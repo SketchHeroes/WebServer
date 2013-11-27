94bc99fb3d377e5b15899248ac480564b84c08b6
Michael Chekin <michael@sketchheroes.com>
2013-11-27 14:43:35 +0200

    started workin on leaderboard navigation menu

--- public_html/js/libs/skhr/header.js
+++ public_html/js/libs/skhr/header.js
@@ -355,6 +355,103 @@ $(function(){
                  $("#popup_leaderboard").fadeOut(); 
                  $("body .overlay").remove();
         }); 
+        
+        
+        // expendable menu buttons
+
+        $( ".category_menu_list > li > img" ).click(function(event) {
+
+            event.stopPropagation(); 
+
+            var li = $(event.target).parent();
+            
+            if( !$("#"+li.attr('id')+" > img").hasClass('active') )
+            {
+                //alert($( ".category_menu_list > li#recent_link > img" ).attr("src"));
+                $( ".category_menu_list > li#recent_link > img" ).attr("src", "images/recent_link_unselected.png");
+                $( ".category_menu_list > li#featured_link > img" ).attr("src", "images/featured_link_unselected.png");
+                $( ".category_menu_list > li#popular_link > img" ).attr("src", "images/popular_link_unselected.png");
+                
+                $("#"+li.attr('id')+" img").attr("src", "images/"+li.attr('id')+"_selected.png");
+                $("#"+li.attr('id')+" img").addClass('active');
+
+                $( ".sub-menu" ).not("#"+li.attr('id')+" .sub-menu").css("display", "none"); 
+                $( ".category_menu_list > li > img" ).not("#"+li.attr('id')+" img").removeClass('active'); 
+            }
+            
+            
+            if( li.hasClass("expandable_period") )
+            {
+                $("#"+li.attr('id')+" .sub-menu").css("display", "block"); 
+            }
+            else
+            {
+                $(".category .gallery").fadeOut();
+                var tutorial_filter = $( ".category_menu_list > li > img.active" ).attr('id');
+                var promise_category_tutorials = getCategoryTutorialsByFilter(category_id, tutorial_filter); 
+                
+                promise_category_tutorials.done(
+                function(data)
+                {
+                    template_generator.category_tutorials = data.tutorials; 
+
+                    var length = template_generator.category_tutorials.length;
+                    last_page = Math.floor((length-1)/(tutorials_per_part*2));
+                    displayPagination(".navigator", nav_pages_length, gallery_page, last_page, first_in_range);
+                    outlinePage(gallery_page);
+                    //alert(last_page);
+                
+                    template_generator.displayTwoPartGallery(   ".category_tutorials .first_part",
+                                                                ".category_tutorials .second_part", 
+                                                                tutorials_per_part, 
+                                                                gallery_page);
+                    $(".category_tutorials").fadeIn();   
+
+                }); 
+            }
+            
+            //alert($( ".category .menu .active" ).attr('id'));
+        });
+
+        $( "html" ).click(function(event) {
+            $( ".sub-menu" ).css("display", "none"); 
+        });  
+
+
+        // user tutorials period buttons
+
+        $( ".expandable_period .sub-menu a" ).click(function(event) {
+
+
+            var period  =  event.target.id;
+
+            //alert(tutorial_filter+":"+period);
+            var tutorial_filter = $( ".category_menu_list > li > img.active" ).attr('id');
+            var promise_category_tutorials;
+
+            if(period === "all_time")
+                promise_category_tutorials = getCategoryTutorialsByFilter(category_id, tutorial_filter); 
+            else
+                promise_category_tutorials = getCategoryTutorialsByFilter(category_id,tutorial_filter, period); 
+
+            promise_category_tutorials.done(
+                    function(data)
+                    {
+                        template_generator.category_tutorials = data.tutorials; 
+
+                        var length = template_generator.category_tutorials.length;
+                        last_page = Math.floor((length-1)/(tutorials_per_part*2));
+                        displayPagination(".navigator", nav_pages_length, gallery_page, last_page, first_in_range);
+                        outlinePage(gallery_page);
+                        //alert(last_page);
+
+                        template_generator.displayTwoPartGallery(   ".category_tutorials .first_part",
+                                                                    ".category_tutorials .second_part", 
+                                                                    tutorials_per_part, 
+                                                                    gallery_page);
+                        $(".category_tutorials").fadeIn();              
+                    }); 
+        });
     });
     
 });
